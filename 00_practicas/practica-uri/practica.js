07f365a297f6a748ec2f629784a17ccf778e6b02
LupitaFernandez <fernandez.guada@hotmail.com>
2020-06-02 21:11:33 -0300

    Last and go on

---
+++ 00_practicas/practica-uri/practica.js
@@ -0,0 +1,20 @@
+//declarada funcion.
+
+
+function numeroImpares (numero){ //primera letra minuscula, segunda palabra mayuscula
+    
+    console.log(numero); 
+    
+    /*
+    for ( var i = 1 ; i >= numero.length ; i++){  
+        if (i % 2 != 0){
+            numero ++;
+        
+        }
+    }
+    return numero;
+    */ //bloque de codigo grande. 
+}
+//control s, para guardar pero el mouse en la pantalla.
+//ejecutar una funcion, los parentesis siempre son necesarios una funcion para ejecutar una funcion
+numeroImpares (23)
